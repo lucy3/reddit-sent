f62fc60c0a301d5d8f81ca242e260ed343683d54
juliamendelsohn <jmendels@stanford.edu>
2018-07-16 23:51:45 +0200

    Add files via upload
    To create sentiment lexicons and vectors for given set of subreddits

--- code/pipeline.py
+++ code/pipeline.py
@@ -4,8 +4,23 @@ from collections import defaultdict
 import scipy.spatial.distance
 from scipy.sparse import coo_matrix,save_npz,load_npz
 from scipy.sparse.linalg import svds, eigs
-import pyximport; pyximport.install()
+import pyximport; pyximport.install() #I don't think this is necessary
+from socialsent import seeds
+from socialsent import lexicons
+from socialsent.polarity_induction_methods import random_walk,label_propagate_probabilistic,_bootstrap_func,bootstrap
+from socialsent.representations.representation_factory import create_representation
+import operator
+import csv
+import glob
+import sys
 import os
+from nltk.corpus import stopwords
+from sklearn import preprocessing
+from operator import itemgetter
+import itertools
+import pickle
+import scipy
+
 
 
 """
@@ -16,18 +31,29 @@ Pipeline
 3. Do PPMI + SVD - place into ppmi_svd_vectors - make sure to write it as [word,v1,v2,...] line (space separated)
 4. Run SocialSent on ppmi_svd_vectors - save to socialsent_lexicons_ppmi_svd 
 5. Create SocialSent vector from socialsent_lexicons_ppmi_svd - save to socialsent_vectors_ppmi_svd (as npy)
-
-
-separately download everything
-make expicit list of subreddit names
-run loop on each subreddit name 
 """
 
+
+""" get_vocabulary
+Gets word counts of subreddit. 
+Assumes that all content from single subreddit is already concatenated into one .txt file, and all subreddit 
+texts are in the same directory (here, referred to as concat_dir)
+All text from subreddits is located within concat_dir directory with the name [subreddit].txt
+Saves vocab information as txt files in vocab_dir
+NOTE: vocab_count bash script is part of the GloVe package.
+"""
 def get_vocabulary(subreddit,concat_dir,vocab_dir):
 	corpus = os.path.join(concat_dir,subreddit + '.txt')
 	vocab = os.path.join(vocab_dir,subreddit + '.txt')
 	os.system('./vocab_count -verbose 2 -max-vocab 100000 -min-count 5 < {} > {}'.format(corpus,vocab))
 
+
+
+""" get_count_matrices
+Gets unweighted cooccurrence matrix for each subreddit using symmetric window size of 4
+Saves as .bin files to cooccur_dir 
+NOTE: cooccur bash script is part of the GloVe package. 
+"""
 def get_count_matrices(subreddit,concat_dir,vocab_dir,cooccur_dir):
 	corpus = os.path.join(concat_dir,subreddit + '.txt')
 	vocab = os.path.join(vocab_dir,subreddit + '.txt')
@@ -35,6 +61,39 @@ def get_count_matrices(subreddit,concat_dir,vocab_dir,cooccur_dir):
 	os.system('./cooccur -verbose 2 -symmetric 1 -window-size 4 -vocab-file {} -memory 8.0 -distance-weighting 0 < {} > {}'.format(vocab,corpus,cooccur_matrix))
 
 
+
+""" prepare_matrix_for_pmi
+Called later in get_ppmi_svd()
+Stores co-occurrence information into numpy matrix 
+Does calculations for some parameters in PPMI calculation
+	- Smoothing parameter (0 in original socialsent)
+	- Context distribution scaling (when True, set to 0.75 as in original socialsent)
+Returns original co-occurrence matrix (old_mat), row_probs, col_probs, and smooth parameter
+NOTE: calculations involving smooth,row_probs, and col_probs are adapted from Will Hamilton's released code
+"""
+def prepare_matrix_for_pmi(data,num_words,smooth=0,cds=True):
+	mat = np.zeros((num_words,num_words))
+	for x,i in enumerate(data):
+		mat[i[1]-1][i[0]-1] = i[2]
+		mat[i[0]-1][i[1]-1] = i[2]
+	old_mat = coo_matrix(mat).tocsr()    
+	smooth = old_mat.sum() * smooth
+	row_probs = old_mat.sum(1) + smooth
+	col_probs = old_mat.sum(0) + smooth
+	if cds:
+		col_probs = np.power(col_probs, 0.75)
+	row_probs = row_probs / row_probs.sum()
+	col_probs = col_probs / col_probs.sum()
+	print('Prepared original matrix for PPMI')
+	return old_mat,row_probs,col_probs,smooth
+
+
+
+""" make_ppmi_mat
+Creates and returns PPMI matrix from original co-occurrence matrix 
+Set to SocialSent default values for neg and normalize 
+NOTE: PPMI calculations adapted from Will Hamilton's released SocialSent code
+"""
 def make_ppmi_mat(old_mat, row_probs, col_probs, smooth, neg=1, normalize=False):
 	prob_norm = old_mat.sum() + (old_mat.shape[0] * old_mat.shape[1]) * smooth
 	old_mat = old_mat.tocoo()
@@ -57,28 +116,15 @@ def make_ppmi_mat(old_mat, row_probs, col_probs, smooth, neg=1, normalize=False)
 	print('Made PPMI matrix')
 	return coo_matrix((data_d, (row_d, col_d)))
 
-def prepare_matrix_for_pmi(data,num_words,smooth=0,cds=True):
-	mat = np.zeros((num_words,num_words))
-	for x,i in enumerate(data):
-		mat[i[1]-1][i[0]-1] = i[2]
-		mat[i[0]-1][i[1]-1] = i[2]
-	old_mat = coo_matrix(mat).tocsr()    
-	#save_npz('mensrights_coo_matrix.npz',coo_mat)
-	#old_mat = load_npz(count_path).tocsr()
-
-	smooth = old_mat.sum() * smooth
-	row_probs = old_mat.sum(1) + smooth
-	col_probs = old_mat.sum(0) + smooth
-	if cds:
-		col_probs = np.power(col_probs, 0.75)
-	row_probs = row_probs / row_probs.sum()
-	col_probs = col_probs / col_probs.sum()
-	print('Prepared original matrix for PPMI')
-	return old_mat,row_probs,col_probs,smooth
 
 
 
-
+""" get_ppmi_svd
+Overarching function to run both PPMI and SVD from co-occurrence matrix 
+SVD set to reduce to 100 dimensions and normalized with L2 norm (normalize set to False in header has to do with PPMI, not SVD)
+Each line of output file consists of word followed by 100-dimension vector (stored as space-separated string)
+Written to file named [subreddit].txt in another directory (here, called ppmi_svd_dir)
+"""
 
 def get_ppmi_svd(subreddit,vocab_dir,cooccur_dir,ppmi_svd_dir,smooth=0,cds=True,neg=1,normalize=False):
 	vocab_file = os.path.join(vocab_dir,subreddit + '.txt')
@@ -90,8 +136,7 @@ def get_ppmi_svd(subreddit,vocab_dir,cooccur_dir,ppmi_svd_dir,smooth=0,cds=True,
 	data = np.fromfile(infile, dtype=dt) 
 	old_mat,row_probs,col_probs,smooth = prepare_matrix_for_pmi(data,len(vocab),smooth,cds)
 	ppmi_mat = make_ppmi_mat(old_mat, row_probs, col_probs,smooth,neg,normalize)
-	# save_npz(out_path,ppmi_mat)
-	# ppmi_mat = load_npz(out_path)
+
 	u, s, vt = svds(ppmi_mat, k=100)
 	l2norm = np.sqrt((u * u).sum(axis=1)).reshape(len(u),1)
 	norm_vecs = u / l2norm
@@ -106,38 +151,151 @@ def get_ppmi_svd(subreddit,vocab_dir,cooccur_dir,ppmi_svd_dir,smooth=0,cds=True,
 	print('Created PPMI-SVD vectors')
 
 
+""" run_sentprop
+Runs sentprop algorithm (largely adapted from released SocialSent)
+Beta value of random walk set to 0.9, number of nearest neighbors set to 25 
+Resulting lexicon stored in socialsent_lexicons_dir 
+topn = n most frequent words for which to induce lexicons
+NOTE: some code adapted from original SocialSent released code including random_walk 
+"""
+def run_sentprop(subreddit,ppmi_svd_dir,socialsent_lexicons_dir,vocab_dir,topn=5000):
+	#program = 'python make_sent_lexicons.py ' + subreddit + " " + ppmi_svd_dir + " " + socialsent_lexicons_dir + " " + vocab_dir
+	#os.system(program)
 
-def run_sentprop(subreddit,ppmi_svd_dir,socialsent_lexicons_dir,vocab_dir):
-	program = 'python make_sent_lexicons.py ' + subreddit + " " + ppmi_svd_dir + " " + socialsent_lexicons_dir + " " + vocab_dir
-	os.system(program)
+	stop_words = set(stopwords.words('english'))
+	stop_words.add('<#S#>') #dummy token 
 
-def make_sent_lexicon(subreddit):
-	vocab_dir = 'vocab_counts'
-	concat_dir = 'concat_subs'
-	cooccur_dir = 'cooccur_matrices_unweighted'
-	ppmi_svd_dir = 'ppmi_svd_vectors'
-	socialsent_lexicons_dir = 'socialsent_lexicons_ppmi_svd_top5000_boots'
-	socialsent_vectors_dir = 'socialsent_vectors_ppmi_svd_boots'
-	# get_vocabulary(subreddit,concat_dir,vocab_dir)
-	# get_count_matrices(subreddit,concat_dir,vocab_dir,cooccur_dir)
-	# get_ppmi_svd(subreddit,vocab_dir,cooccur_dir,ppmi_svd_dir)
+	fname = os.path.join(vocab_dir,subreddit + '.txt')
+	with open(fname,'r') as f:
+		words = f.readlines()
+
+	top_words = [w.split()[0] for w in words if w not in stop_words][:topn] 
+	pos_seeds, neg_seeds = seeds.twitter_seeds() #Twitter seed words (from socialsent package)
+
+    vector_file = os.path.join(ppmi_svd_dir,subreddit + '.txt')
+    embeddings = create_representation('GIGA', vector_file,
+        set(top_5000).union(pos_seeds).union(neg_seeds))
+
+  	# with random_walk, num_boots doesnt do anything. Need to change to bootstrap
+	polarities = random_walk(embeddings, pos_seeds, neg_seeds, beta=0.9, nn=25,num_boots=50,n_procs=10)
+    sorted_x = sorted(polarities.items(), key=operator.itemgetter(1))
+    outfile = os.path.join(socialsent_lexicons_dir,subreddit + '.txt')
+
+    with open(outfile,'w') as f:
+        tsvin = csv.writer(f,delimiter='\t')
+        for word in sorted_x:
+            tsvin.writerow(word)
+
+
+
+""" make_sent_lexicon
+Full pipeline to run everything for one subreddit
+Specify all relevant directories 
+"""
+def make_sent_lexicon(subreddit,concat_dir,vocab_dir,cooccur_dir,
+	ppmi_svd_dir,socialsent_lexicons_dir,socialsent_vectors_dir):
+	
+	get_vocabulary(subreddit,concat_dir,vocab_dir)
+	get_count_matrices(subreddit,concat_dir,vocab_dir,cooccur_dir)
+	get_ppmi_svd(subreddit,vocab_dir,cooccur_dir,ppmi_svd_dir)
 	run_sentprop(subreddit,ppmi_svd_dir,socialsent_lexicons_dir,vocab_dir)
 
 
 """ 
-Making the sentprop vectors requires all the lexicons so it must wait
-It involves combining the vocabulary, normalizing (look back at the paper) and saving the vector
+All functions below are for creating subreddit sentiment vectors from the lexicon
 """
 
-def make_sentprop_vectors(subreddit,socialsent_lexicons_dir,socialsent_vectors_dir):
-	return 0 
+
+"""
+Normalizes sentiment lexicon to have 0 mean and unit variance (as in original paper)
+Returns lexicon as a dict containing {word:normalized score} k,v pairs 
+"""
+def normalize_lexicon(socialsent_lexicon_dir,subreddit):
+	sent_dict = {}
+	scaled_sent_dict = {}
+	infile_path = os.path.join(socialsent_lexicon_dir, subreddit + '.txt')
+	with open(infile_path,'r') as f:
+		tsvin = csv.reader(f,delimiter='\t')
+		for line in tsvin:
+			sent_dict[line[0]] = float(line[1])
+	words_ordered = sorted(sent_dict.keys())
+	sents_ordered = [sent_dict[w] for w in words_ordered]
+	sents_ordered_norm = preprocessing.scale(sents_ordered)
+
+	for i in range(len(words_ordered)):
+		w = words_ordered[i]
+		scaled_sent_dict[w] = float(sents_ordered_norm[i])
+	return scaled_sent_dict
+
+
+"""
+For a specific given subreddit, returns the set of words for which sentiment was induced 
+"""
+def get_vocab(socialsent_lexicons_dir,subreddit):
+	vocab = []
+	infile_path = os.path.join(socialsent_lexicons_dir, subreddit + '.txt')
+	with open(infile_path,'r') as f:
+		tsvin = csv.reader(f,delimiter='\t')
+		for line in tsvin:
+			vocab.append(line[0])
+	return set(vocab)
+
+"""
+Given the full vocabulary (set of all words that have sentiment in ANY subreddit), creates vector 
+Adds 0.0 for any word that does not have a sentiment value in specific subreddit
+"""
+def get_vector_for_lexicon(vocab,scaled_sent_dict):
+	vec_list = []
+	for w in vocab:
+		if w in scaled_sent_dict:
+			vec_list.append(scaled_sent_dict[w])
+		else:
+			vec_list.append(0.0)
+	return np.array(vec_list)
+
+
+
+"""
+Creates sentiment vectors for all subreddits listed in subreddit_names after lexicon induction 
+Saves vectors as .np files to socialsent_vectors_dir 
+"""
+def make_sentprop_vectors(subreddit_names,socialsent_lexicons_dir,socialsent_vectors_dir):
+	full_vocab = set()
+	for subreddit in subreddit_names:
+		full_vocab = full_vocab | get_vocab(socialsent_lexicons_dir,subreddit)
+	vocab_ordered = sorted(full_vocab)
+	with open('vocab_socialsent_ordered.pkl', "wb") as output_file:
+		pickle.dump(vocab_ordered, output_file)
+
+	for subreddit in subreddit_names:
+		scaled_sent_dict = normalize_lexicon(socialsent_lexicons_dir,subreddit)
+		vector = get_vector_for_lexicon(vocab_ordered,scaled_sent_dict)
+		filename = os.path.join(socialsent_vectors_dir,subreddit + '.npy')
+		np.save(filename,vector)
+
+
+
+
+
 
 def main():
-	#subreddit_list = ['femalefashionadvice']
-	subreddit_list = ['femalefashionadvice','malefashionadvice','mensrights','trollxchromosomes','london','actuallesbians','askmen','askwomen','askgaybros','xxfitness']
+	vocab_dir = 'vocab_counts'
+	concat_dir = 'concat_subs'
+	cooccur_dir = 'cooccur_matrices_unweighted'
+	ppmi_svd_dir = 'ppmi_svd_vectors'
+	sent_lexicons_dir = 'socialsent_lexicons_ppmi_svd_top5000'
+	sent_vectors_dir = 'socialsent_vectors_ppmi_svd_top5000'
+
+	#all gender subreddits ('london' no longer included)
+	subreddit_list = ['femalefashionadvice','malefashionadvice','mensrights','trollxchromosomes','actuallesbians','askmen','askwomen','askgaybros','xxfitness']
+	
+	#makes sentiment lexicons for each subreddit
 	for subreddit in subreddit_list:
 		print(subreddit)
-		make_sent_lexicon(subreddit)
+		make_sent_lexicon(subreddit,concat_dir,vocab_dir,cooccur_dir,ppmi_svd_dir,sent_lexicons_dir,sent_vectors_dir)
+
+	#makes vectors for each subreddit after lexicon induction
+	make_sentprop_vectors(subreddit_list,sent_lexicons_dir,sent_vectors_dir)
 
 if __name__ == "__main__":
 	main()
