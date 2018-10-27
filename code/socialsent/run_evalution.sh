7ad41c963ba406043d1a82d96c39e7a95fb0c516
lucy3 <lucy3@stanford.edu>
2018-10-27 10:49:26 -0700

    remove socialsent

--- code/socialsent/run_evalution.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python evaluate_methods.py standard > results/standard.txt 
-python evaluate_methods.py finance > results/finance.txt 
-python evaluate_methods.py twitter > results/twitter.txt 
-python evaluate_methods.py adj > results/adjectives.txt 
-python evaluate_methods.py overlap > results/overlap.txt 
